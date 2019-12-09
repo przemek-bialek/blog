db6afb86809400da5f9d2c774e104acd8b12c655
Przemek Białek <prz.bialek1998@gmail.com>
2019-12-09 17:33:30 +0100

    Added first templates: base, home and about

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'user1',
+        'title': 'first post',
+        'content': 'first post content',
+        'date_posted': '11 Dec 2019'
+    },
+    {
+        'author': 'user2',
+        'title': 'second post',
+        'content': 'second post content',
+        'date_posted': '12 Dec 2019'
+    }
+]
 
 def home(request):
-    return HttpResponse("my_blog home")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
