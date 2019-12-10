ffc8709dc5d54e2f27ecd60b1fc821fde2c850dd
Przemek Białek <prz.bialek1998@gmail.com>
2019-12-10 18:10:02 +0100

    Created new app: User; along with user registration

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from blog import urls
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('user/', include('user.urls')),
 ]
