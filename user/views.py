f8b43be9d4f706b8d509bb9d7422b567f8ac973f
Przemek Białek <prz.bialek1998@gmail.com>
2019-12-11 17:56:32 +0100

    Added more user functionality: login, logout options and basic profile view

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .forms import UserRegisterForm
 
@@ -8,9 +9,12 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account has been successfully created for {username}')
-            return redirect('blog-home')
+            messages.success(request, 'Account has been successfully created. You can now login')
+            return redirect('user-login')
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
