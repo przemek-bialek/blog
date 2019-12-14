594bb9cf4f75bbd510ef8d4b1d1c1332584cd20a
Przemek Białek <prz.bialek1998@gmail.com>
2019-12-14 13:37:26 +0100

    Better img handling and  ability to update one's profile

--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}\'s profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.img.path)
+
+        if img.width > 250:
+            if img.height > 250:
+                img.thumbnail((250, 250))
+            else:
+                img.thumbnail((250, img.height))
+        else:
+            if img.height > 250:
+                img.thumbnail((img.height, 250))
+        img.save(self.img.path)
