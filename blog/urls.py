37efb1201efcff80b4dc8812567e791affd78dda
Przemek Białek <prz.bialek1998@gmail.com>
2019-12-17 11:31:08 +0100

    Pagination on posts listing and ability to view all user's posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
+    path('<str:username>/', views.UserPostListView.as_view(), name='blog-user_posts'),
     path('post/create/', views.PostCreateView.as_view(), name='blog-post_form'),
     path('post/<slug:slug>/', views.PostDetailView.as_view(), name='blog-post_detail'),
     path('post/<slug:slug>/update/', views.PostUpdateView.as_view(), name='blog-post_update'),
